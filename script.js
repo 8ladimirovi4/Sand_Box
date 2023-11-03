1a4518290e07be8772331764ed026de2e84880c2
vlleo <vladimir.leonov@systeme.ru>
2023-11-03 12:16:20 +0300

    удалил лишние файлы

--- script.js
+++ script.js
@@ -1,248 +1,194 @@
 
-
-document.addEventListener("DOMContentLoaded", function() {
-//--расширенный hint(автозаполнение)----------------->
-  function getURL(url, c) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("get", url, true);
-    xhr.send();
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState != 4) return;
-      if (xhr.status < 400) return c(null, xhr.responseText);
-      var e = new Error(xhr.responseText || "No response");
-      e.status = xhr.status;
-      c(e);
-    };
-  }
-
-  var server;
-  getURL("codemirror/addon/tern/defs/ecmascript.json", function(err, code) {
-    if (err) throw new Error("Request for ecmascript.json: " + err);
-    server = new CodeMirror.TernServer({defs: [JSON.parse(code)]});
-    editor.setOption("extraKeys", {
-      "Ctrl-Space": function(cm) { server.complete(cm); },
-      "Ctrl-I": function(cm) { server.showType(cm); },
-      "Ctrl-O": function(cm) { server.showDocs(cm); },
-      "Alt-.": function(cm) { server.jumpToDef(cm); },
-      "Alt-,": function(cm) { server.jumpBack(cm); },
-      "Ctrl-Q": function(cm) { server.rename(cm); },
-      "Ctrl-.": function(cm) { server.selectName(cm); }
-    })
-    editor.on("cursorActivity", function(cm) { server.updateArgHints(cm); });
-  });
-//<--расширенный hint(автозаполнение)-----------------
-
-
-var editor = CodeMirror.fromTextArea(document.getElementById("code"), {
-    mode: "javascript",
-    theme: "default",
-    lineNumbers: true,
-    extraKeys: {
-        "Ctrl-Space": "autocomplete",
-        "F11": function(cm) {
-            cm.setOption("fullScreen", !cm.getOption("fullScreen"));
-          },
-          "Esc": function(cm) {
-            if (cm.getOption("fullScreen")) cm.setOption("fullScreen", false);
-          },
-          "Alt-F": "findPersistent",
-    },
-    autoCloseBrackets: true,
-    gutters: [
-        "CodeMirror-lint-markers", //линтер
-        "CodeMirror-linenumbers", //breake points
-        "breakpoints" //breake points
-    ],
-    //линтер
-    lint: {options: {esversion: 2021}}
-  });
-
-  //---breake points--------------------------------->
-  editor.on("gutterClick", function(cm, n) {
-    var info = cm.lineInfo(n);
-    cm.setGutterMarker(n, "breakpoints", info.gutterMarkers ? null : makeMarker());
-  });
+var timeConditionConfig = {
+  id: 'timeConditionCfg',
+ 
   
-  function makeMarker() {
-    var marker = document.createElement("div");
-    marker.style.color = "#822";
-    marker.innerHTML = "●";
-    return marker;
-  }
-  //<---breake points---------------------------------
-
-  //----поиск---------------------------------------->
-// Функция для поиска и выделения текста
-function findAndSelectText(inputs) {
-    const searchText = inputs[0].value;
-    const content = editor.getValue();
-    const startIndex = content.indexOf(searchText);
-  
-    if (startIndex !== -1) {
-      // Найдено совпадение
-        editor.setSelection(
-        editor.posFromIndex(startIndex),
-        editor.posFromIndex(startIndex + searchText.length)
-      );
-    } else {
-      // Совпадение не найдено
-      window.alert("Совпадений не найдено.");
-    }
-  }
-
-  // Создаем шаблон диалогового окна
-const dialogTemplate = document.createElement("div");
-dialogTemplate.innerHTML = `
-  <label for="searchText">Текст для поиска:</label>
-  <input type="text" id="searchText">
-  <button id="searchButton">Найти</button>
-`;
-
-// Опции для диалогового окна
-const dialogOptions = {
-    shrinkEditor: true,
-    inputBehaviours: [
-      {
-        closeOnEnter: true,
-        closeOnBlur: false,
-        callback: findAndSelectText
-      }
-    ],
-    buttonBehaviours: [
-        {
-          callback: () => {
-            const inputs = dialogTemplate.getElementsByTagName("input");
-            findAndSelectText(inputs);
-          }
-        }
+      rows: [
+        { view: 'label', label: 'Период вызова скрипта:' },
+        { id: 'counterDays', name: 'schedule.time.period.span.d', view: 'counter', label: 'дни', value: 0, step: 1, value: 0, min: 0, max: 366, labelWidth: 90 },
+        { id: 'counterHours', name: 'schedule.time.period.span.h', view: 'counter', label: 'часы', step: 1, value: 1, min: 0, max: 23, labelWidth: 90 },
+        { id: 'counterMinutes', name: 'schedule.time.period.span.m', view: 'counter', label: 'минуты', step: 1, value: 0, min: 0, max: 59, labelWidth: 90 },
+        { id: 'counterSeconds', name: 'schedule.time.period.span.s', view: 'counter', label: 'секунды', step: 1, value: 0, min: 0, max: 59, labelWidth: 90 },
       ],
-    onClose: (dialog) => {
-      dialog.remove();
-    }
-  };
 
-// Функция для открытия диалогового окна поиска
-function openSearchDialog() {
-    var modalBox = document.querySelector(".CodeMirror-advanced-dialog");
-        if(modalBox){
-        modalBox.classList.remove('modalHidden') 
-        modalBox.classList.add('modalViaible')
-        return
-        }
-    const dialog = editor.openAdvancedDialog(dialogTemplate, dialogOptions);
   }
+//
+var schedulePeriodTypes = [
+  { id: 1, value: 'Период', b: 'PerHour' },
+  { id: 2, value: 'Неделя', b: 'PerWeek' },
+  { id: 3, value: 'Год', b: 'PerYear' },
+];
 
-  const searchButton = document.getElementById("searchButton");
-  searchButton.addEventListener("click", openSearchDialog); 
+var timeCondition = {
+  rows: [
+    {
+      id: 'timeSchedulePeriodType',
+      name: 'schedule.time.period.type',
+      view: 'radio',
+      value: 1,
+      defaultValue: 1,
+      options: schedulePeriodTypes,
+      vertical: false,
+      on: {
+        onChange: function (newv, oldv) {
+          var choice = this.config.options.find((x) => x.id.toString() === newv);
+          if (choice) $$(timeConditionConfig.id).showBatch(choice.b);
+        },
+      },
+    },
+    {
+      name: 'schedule.time.t_postpon', //old name - schedule.time.period.t_postpon
+      label: 'Смещение времени старта:',
+      view: 'select',
+      options: [],
+      inputWidth: 300,
+      labelWidth: 200,
+      tooltip: 'Смещение времени старта отсчёта периода от полуночи по UTC',
+      defaultValue: '03:00:00',
+    },
+    { height: 10 },
+    timeConditionConfig,
+  ],
+};
 
-  var closeModalButton = document.getElementById("closeButton");
-  
-  closeModalButton.addEventListener('click', function(){
-       
-    var modalBox = document.querySelector(".CodeMirror-advanced-dialog");
-    if(modalBox){
-        modalBox.classList.remove('modalViaible')
-    modalBox.classList.add('modalHidden') 
-    }
+
+var signUseBody = {
+  id: 'signUseBody',
+  height: 200,
+  width: 850,
+  cols: [
+    {
+      width: 600,
+      css: 'sign_expression',
+      inputWidth: 600,
+      labelWidth: 600,
+      name: 'schedule.sign.expr',
+      id: 'signExpression',
+      view: 'textarea',
+      label: 'Условие, при котором будет выполнен скрипт:',
+      labelPosition: 'top',
+      defaultValue: '',
+
+    },
+  ],
+};
+
+var apertureUseBody = {
+  id: 'apertureUseBody',
+  height: 200,
+  width: 850,
+  cols: [
+      {
+          id: "signals",
+          view: "datatable",
+          resizeColumn: { size: 8 },
+          resizeRow: false,
+         
+          headerRowHeight: 30,
+          navigation: true,
+          editaction: "dblclick",
+          select: "cell",
+          rowHeight: 24,
+          drag: false,
+          dragColumn: "order",
+          scrollX: true,
+          checkboxRefresh: true,
+          columns:
+              [
+                  {
+                      id: "signal", header: "Сигнал", width: 350, editor: "text", sort: "string", fillspace: true,
+                  },
+                  {
+                      id: "deviation", header: "Отклонение", width: 150, editor: "text", fillspace: true, number: true
+                  },
+                  {
+                      id: "trash", header: "<span class='webix_icon wxi-trash remove_all' title='fff'></span>", width: 35,
+                     
+                  }
+              ],
+          data:[],
+          
+          
+      }
+  ],
+};
+
+webix.ui({
+  view: 'scrollview',
+  body: {
+  rows:[
+{
+  view: 'form',
+  cols: [
+              {
+                rows: [
+                  { name: 'name', label: 'Название:', view: 'text' },
+                  { name: 'desc', label: 'Описание:', view: 'text' },
+                  { view: 'label', label: 'Расписание:' },
+                 {
+                    view: "fieldset",
+                    label: "<div id='fooo'></div>",
+                    css: 'my-fieldset', // Добавляем пользовательский CSS класс
+                    body: {
+                        rows: [
+                         timeCondition
+                        ]
+                    }
+                },
+                  {
+                    view: 'fieldset',
+                    id: 'signUse',
+                    css: 'my-fieldset', // Добавляем пользовательский CSS класс
+                    label: "<div id='fooo1'></div>",
+                      body: {
+                          rows: [
+                            signUseBody
+                          ]
+                      }
+                  },
+                  {
+                    view: 'fieldset',
+                    id: 'apertureUse',
+                    css: 'my-fieldset', // Добавляем пользовательский CSS класс
+                    label: "<div id='fooo2'></div>",
+                      body: {
+      rows:[
+        apertureUseBody
+      ]
+  },
+ height: 200,
+                  },
+                ],
+              }
+            ]
+}
+  ]
+
+  }
   })
-//<----поиск----------------------------------------
 
 
-//   var searchButton = document.getElementById("searchButton");
-//   var replaceButton = document.getElementById("replaceButton");
-//   var closeModalButton = document.getElementById("closeButton");
-// Обработчик события для кнопки "Поиск"
 
-
-// closeModalButton.addEventListener("click", function () {
-//     var modalBox = document.querySelector(".CodeMirror-advanced-dialog");
-// if(modalBox)
-// modalBox.classList.remove('modalViaible') 
-// modalBox.classList.add('modalHidden')
-// })
-
-
-// searchButton.addEventListener("click", function () {
-//     var modalBox = document.querySelector(".CodeMirror-advanced-dialog");
-//     if(modalBox){
-//     modalBox.classList.remove('modalHidden') 
-//     modalBox.classList.add('modalViaible')
-//     return
-//     }
-
-//     var searchTemplate = document.createElement("div");
-//     searchTemplate.classList.add('searchTemplate')
-//     const input = document.createElement("input")
-//     input.id = "searchInput"
-//     input.classList.add('search_Input')
-//     input.setAttribute("type", "text")
-//     searchTemplate.appendChild(input)
-//     const button = document.createElement("button")
-//     button.id = "startSearch"
-//     button.textContent = 'Начать поиск'
-//     searchTemplate.appendChild(button)
-// const arr = []
-// arr.push(input)
-
-
-//      editor.openAdvancedDialog(searchTemplate, {
-//       shrinkEditor: true,
-//       inputBehaviours: 
-//         {
-//           closeOnEnter: true,
-//           closeOnBlur: true,
-//           callback: function (arr, event) {
-//             console.log(arr)
-           
-//             performSearch(arr[1].value);
-           
-//           },
-//         },
-    
-//         buttonBehaviours:{
-//             callback: function (arr, event) {
-//                 console.log(arr)
-               
-//                 performSearch(arr[1].value);
-               
-//               },
-//         },
-//       onClose: function () {
-//         searchTemplate.remove();
-//       },
+//   webix.ui({
+//     container:'fooo',
+//       view:"checkbox", 
+//       id:"field_a", 
+//       label:"Second age", 
+//       value:1,
 //     });
+
+// webix.ui({
+//   container:'fooo1',
+//     view:"checkbox", 
+//     id:"field_a1", 
+//     label:"Second age", 
+//     value:1,
 //   });
 
-  
-
-//   const searchInput = document.getElementById('search-input');
-//   searchInput.addEventListener('input', () => {
-//     const searchTerm = searchInput.value;
-//     const code = editor.getValue();
-    
-//     const cursor = editor.getSearchCursor(searchTerm);
-//     cursor.findNext()
-//   })
-
-
-// var searchButton = document.getElementById("searchButton");
-// var findNextButton = document.getElementById("findNextButton");
-// var findPrevButton = document.getElementById("findPrevButton");
-
-// // Обработчик события для кнопки "Поиск"
-// searchButton.addEventListener("click", function () {
-//   editor.execCommand("findPersistent");
-// });
-
-// // Обработчик события для кнопки "Найти следующее"
-// findNextButton.addEventListener("click", function () {
-//   editor.execCommand("findNext");
-// });
-
-// // Обработчик события для кнопки "Найти предыдущее"
-// findPrevButton.addEventListener("click", function () {
-//   editor.execCommand("findPrev");
-// });
-
-})
-
+//   webix.ui({
+//     container:'fooo2',
+//       view:"checkbox", 
+//       id:"field_a2", 
+//       label:"Second age", 
+//       value:1,
+//     });
