069eb69f47be2ec5511ac239fa2690d6334d9f28
vlleo <vladimir.leonov@systeme.ru>
2023-11-16 14:12:22 +0300

    добавил правило @media print

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//
+
+
+// for(let i = 0; i < 100; i++){
+// const block = document.createElement('div')
+// block.classList = 'container'
+// block.style.width = '100%'
+// block.style.height = '50px'
+// block.style.border = '1px solid black'
+// block.style.display = 'flex'
+// block.style.alignItems = 'center'
+
+// const innerBlock = document.createElement('div')
+// innerBlock.classList = 'inner'
+// innerBlock.innerText = 'Давно выяснено, что при оценке дизайна и композиции'
+// block.appendChild(innerBlock)
+// document.body.appendChild(block)
+// }
