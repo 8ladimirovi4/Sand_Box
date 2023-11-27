98852334e8e12392ff054cb1ae35f9ba011647c1
vlleo <vladimir.leonov@systeme.ru>
2023-11-27 17:12:18 +0300

    создал несколько вариантов таблиц

---
+++ example_table.js
@@ -0,0 +1,91 @@
+const data = []
+
+
+
+var datatable = webix.ui({
+    container: 'data_table',
+    view: 'layout',
+rows:[
+    {
+    container: 'data_table',
+    view:"datatable", 
+    scroll: false,
+    id:'dt',
+    autoheight: true,
+    columns:[
+        { id:"rank",    
+        header:"",
+        width:200,
+    },
+
+        { id:"title",   
+        header:"Film title",   
+         width:500
+        },
+
+        { id:"year",    
+        header:"Released",      
+        width:200
+    },
+
+        { id:"votes",   
+        header:"Votes",         
+        width:200
+    }
+
+    ],
+    data: [],
+    ready: function(){
+        for(let i = 0; i < 300; i++){
+
+            let title = null
+            let year = null
+            let votes = null
+
+            const flag = {
+                title: Math.round(Math.random()),
+                year: Math.round(Math.random()),
+                votes: Math.round(Math.random())
+               
+            } 
+
+           
+          
+            if (flag.title === 1) {
+                title = "The Shawshank Redemption";
+            }
+        
+            if (flag.year === 1) {
+                year = `19${i}`;
+            }
+        
+            if (flag.votes === 1) {
+                votes = `6787${i}`;
+            }
+
+             data.push(
+                 { id:i, title, year, votes, rank:i, tags: [
+                    {
+                        "n": "Тест WebScadaMT\\IEC-104\\Измерения\\Оперативная информация\\Ia",
+                        "a": "X0",
+                        "s": {
+                            "sid": "71fef400-b377-4767-9c9f-e67c50684ef1",
+                            "q": 192,
+                            "v": "4.439497470855713",
+                            "record_ts": "2023-11-21T12:14:02.1163617Z",
+                            "ts": "2023-11-21T12:12:04.2960000Z"
+                        },
+                        "vtype": 14
+                    }
+                ],}
+             )
+         
+         }
+         this.define('data', data);
+        
+
+    },
+}]
+});
+
+//$$('dt').adjustRowHeight();
---
+++ table_pure.js
@@ -0,0 +1,56 @@
+const data = []
+
+
+
+var datatable = webix.ui({
+    container: 'data_table',
+    id:'lay',
+    view: 'layout',
+rows:[
+    {
+    container: 'data_table',
+    view:"datatable",
+    id:'dt',
+    autoheight: true,
+    columns:[
+        { id:"rank",    
+        header:"",
+        width:200,
+    },
+
+        { id:"title",   
+        header:"Film title",   
+         width:500
+        },
+
+        { id:"year",    
+        header:"Released",      
+        width:200
+    },
+
+        { id:"votes",   
+        header:"Votes",         
+        width:200
+    }
+
+    ],
+
+
+    data: [
+
+    ],
+        ready: function(){
+        for(let i = 0; i < 300; i++){
+            data.push(
+                { id:i, title:"The Shawshank Redemption", year:1994, votes:678790, rank:i},
+            )
+        }
+        this.define('data', data);
+        this.adjustRowHeight()
+    }
+    }   
+]
+})
+
+
+
