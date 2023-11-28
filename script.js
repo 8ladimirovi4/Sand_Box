6ec0b134a14dfbc3bee200b793e56803b6d84361
vlleo <vladimir.leonov@systeme.ru>
2023-11-28 18:12:38 +0300

    first_commit

--- script.js
+++ script.js
@@ -1,48 +1,24 @@
-const data = []
+function checkReading() {
+  if (checkReading.read) {
+    return;
+  }
+  checkReading.read =
+    this.scrollHeight - Math.round(this.scrollTop) === this.clientHeight;
+  document.registration.accept.disabled = document.getElementById(
+    "nextstep",
+  ).disabled = !checkReading.read;
+  checkReading.noticeBox.textContent = checkReading.read
+    ? "Thank you."
+    : "Please, scroll and read the following text.";
+}
 
-
-var datatable = webix.ui({
-    container: 'data_table',
-    view: 'layout',
-rows:[
-    {
-    container: 'data_table',
-    view:"datatable", 
-    id:'dt',
-    autoheight: true,
-    columns:[
-        { id:"rank",    header:"",              width:200,},
-        { id:"title",   header:"Film title",    width:500},
-        { id:"year",    header:"Released",      width:200},
-        { id:"votes",   header:"Votes",         width:200}
-    ],
-    data: [],
-    ready: function(){
-        for(let i = 0; i < 300; i++){
-           
-             data.push(
-                 { id:i, title:"The Shawshank Redemption", year:`19${i}`, votes:678790, rank:i, tags: [
-                    {
-                        "n": "Тест WebScadaMT\\IEC-104\\Измерения\\Оперативная информация\\Ia",
-                        "a": "X0",
-                        "s": {
-                            "sid": "71fef400-b377-4767-9c9f-e67c50684ef1",
-                            "q": 192,
-                            "v": "4.439497470855713",
-                            "record_ts": "2023-11-21T12:14:02.1163617Z",
-                            "ts": "2023-11-21T12:12:04.2960000Z"
-                        },
-                        "vtype": 14
-                    }
-                ],}
-             )
-         
-         }
-         this.define('data', data);
-        
-
-    },
-}]
-});
-
-//$$('dt').adjustRowHeight();
+onload = () => {
+  const oToBeRead = document.getElementById("rules");
+  checkReading.noticeBox = document.createElement("span");
+  document.registration.accept.checked = false;
+  checkReading.noticeBox.id = "notice";
+  oToBeRead.parentNode.insertBefore(checkReading.noticeBox, oToBeRead);
+  oToBeRead.parentNode.insertBefore(document.createElement("br"), oToBeRead);
+  oToBeRead.onscroll = checkReading;
+  checkReading.call(oToBeRead);
+};
