da21512bc92359633f666b5e948b449112dd9de7
vlleo <vladimir.leonov@systeme.ru>
2023-12-06 13:55:01 +0300

    Добавил верменную шкалу с динамическим изменением временных отрезков (не работает ввод данных)

--- script.js
+++ script.js
@@ -1,71 +1,122 @@
 const data = []
 const scale = []
-let valueX = null
-let valueY = null
+let xScaleFuture = 5
+let xScalePast = 5
+let timeUnits = 'day'
+let step = 1
+
 // Получение ссылки на элемент canvas
 const canvas = document.getElementById('myChart');
 
 // Создание графика с конфигурацией
 const ctx = canvas.getContext('2d');
 const chart = new Chart(ctx, {
-  type: 'line', // Тип графика (линейный)
-  data: {
-    labels: scale, // Массив меток по оси X (пока пустой)
-    datasets: [
-      {
-        label: 'График данных',
-        data: data, // Массив данных
-        borderColor: 'rgb(75, 192, 192)', // Цвет линии
-        borderWidth: 2, // Ширина линии
-        fill: false, // Заполнение под линией выключено
-      },
-    ],
-  },
-  options: {
-    responsive: true,
-    maintainAspectRatio: false,
-    scales: {
-      x: {
-        display: true,
-        type: 'time',
-        autoSkip: false,
-        position: 'bottom',
-        distribution: 'linear',
-        bounds: 'ticks',
-        time: {
-          isoWeekday: true,
-          unit: 'day',
-          //unitStepSize: 10,
-          //stepSize: 1,
-          //precision: 1,
-          //round: true,
-          //minUnit: 'hour',
-          displayFormats: {
-            millisecond: 'ss.SSS',
-            second: 'mm:ss.SSS',
-            minute: 'HH:mm:ss',
-            hour: 'DD.MM HH:mm',
-            day: 'DD.MM.YYYY',
-            week: 'WW.YYYY',
-            month: 'DD.MM.YYYY',
-            quarter: 'Q YYYY',
-            year: 'YYYY',
-          },
+    type: 'scatter',
+    data: {
+      datasets: [
+        {
+          label: 'Тренд 1',
+          fill: false,
+          showLine: true,
+          lineTension: 0,
+          backgroundColor: 'lightgrey',
+          borderColor: 'lightgrey',
+          data: [],
         },
-        min: '1990-11-06 23:39:30',          
-        max: '2230-11-06 23:39:30',  
-      },
-      y: {
-        beginAtZero: false, // Начинать шкалу Y не с нуля
-        min: -10,          // Минимальное значение шкалы Y
-        max: 10,           // Максимальное значение шкалы Y
-        stepSize: 1,       // Шаг шкалы Y
-      },
+        {
+          label: 'Тренд 2',
+          fill: false,
+          showLine: true,
+          lineTension: 0,
+          backgroundColor: 'lightgrey',
+          borderColor: 'lightgrey',
+          data: [],
+        },
+      ],
     },
-  },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      animation: {
+        duration: 300,
+      },
+      title: {
+        display: true,
+        text: 'График',
+      },
+      legend: {
+        display: true,
+        position: 'top',
+        onClick: function () {},
+        labels: {
+          boxWidth: 15,
+          fontFamily: 'Arial',
+          fontSize: 8,
+          padding: 5,
+          usePointStyle: false,
+        },
+      },
+      scales: {
+        xAxes: 
+          {
+            display: true,
+            type: 'time',
+            autoSkip: false,
+            position: 'bottom',
+            distribution: 'linear',
+            bounds: 'ticks',
+            time: {
+              isoWeekday: true,
+              unit: 'day',
+              displayFormats: {
+                millisecond: 'ss.SSS',
+                second: 'mm:ss.SSS',
+                minute: 'HH:mm:ss',
+                hour: 'DD.MM HH:mm',
+                day: 'DD.MM',
+                week: 'WW.YYYY',
+                month: 'DD.MM.YYYY',
+                quarter: 'Q YYYY',
+                year: 'YYYY',
+              },
+            },
+            scaleLabel: {
+              display: true,
+              labelString: 'Время',
+              lineHeight: 1,
+              fontFamily: 'Arial',
+              fontSize: 10,
+              padding: { bootom: 5 },
+            },
+          },
+        
+        yAxes: 
+          {
+            display: true,
+            scaleLabel: {
+              display: true,
+              labelString: 'Значение',
+              lineHeight: 1,
+              fontFamily: 'Arial',
+              fontSize: 10,
+              padding: { top: 5 },
+            },
+            ticks: {
+              source: 'data',
+              maxTicksLimit: 10,
+              //stepSize: 1,
+              precision: 3,
+              lineHeight: 1,
+              fontFamily: 'Arial',
+              fontSize: 10,
+            },
+          },
+        
+      },
+    }
 });
 
-function scaleX(valX, valY){
+function scaleX(xScaleRight, xScaleLeft, xUnit,  xStep){
 // Получаем текущие данные
 const currentData1 = chart.data.datasets[0].data;
 
@@ -78,14 +129,66 @@ const lastIndex = currentData1.length - 1;
 // Получаем значение X для последней точки
 const lastXValue = currentLabels1[lastIndex];
 
-chart.options.scales.x.max = valX ? lastXValue + Number(valX) : chart.options.scales.x.max
 
-chart.options.scales.x.min = valY ? Number(valY) : chart.options.scales.x.min
+
+if (xScaleLeft > 0 && xStep > 0) {
+  let minDate = moment()
+  let maxDate = moment()
+
+  switch (xUnit) {
+      case 'millisecond':
+          minDate.subtract(xScaleLeft, 'millisecond');
+          maxDate.add(xScaleRight, 'millisecond');
+          break;
+      case 'second':
+          minDate.subtract(xScaleLeft, 'seconds');
+          maxDate.add(xScaleRight, 'seconds');
+          break;
+      case 'minute':
+          minDate.subtract(xScaleLeft, 'minutes');
+          maxDate.add(xScaleRight, 'minutes');
+          break;
+      case 'hour':
+          minDate.subtract(xScaleLeft, 'hours');
+          maxDate.add(xScaleRight, 'hours');
+          break;
+      case 'day':
+          minDate.subtract(xScaleLeft, 'days');
+          maxDate.add(xScaleRight, 'days');
+          break;
+      case 'week':
+          minDate.subtract(xScaleLeft, 'weeks');
+          maxDate.add(xScaleRight, 'weeks');
+          break;
+      case 'month':
+          minDate.subtract(xScaleLeft, 'months');
+          maxDate.add(xScaleRight, 'months');
+          break;
+      case 'year':
+          minDate.subtract(xScaleLeft, 'years');
+          maxDate.add(xScaleRight, 'years');
+          break;
+      default:
+          break;
+  }
+
+  chart.options.scales.xAxes.time.stepSize = xStep
+  chart.options.scales.xAxes.time.unit = xUnit
+  console.log(minDate.format('YYYY-MM-DD HH:mm:ss'))
+  console.log(maxDate.format('YYYY-MM-DD HH:mm:ss'))
+
+  if (minDate != null) chart.options.scales.xAxes.min = minDate;
+  if (maxDate != null) chart.options.scales.xAxes.max = maxDate;
+
+  console.log(chart.options.scales.xAxes.min)
+  console.log(chart.options.scales.xAxes.max)
+}
+
 
 chart.update();
 }
 
-scaleX()
+scaleX(xScaleFuture, xScalePast, timeUnits, step)
 
 // Функция для добавления данных в график
 function addData() {
@@ -108,7 +211,7 @@ function addData() {
   // Очищаем инпут
   inputElement.value = '';
 
-  scaleX(valueX,valueY )
+  scaleX(xScaleFuture,xScalePast,timeUnits, step)
 }
 
 
@@ -118,17 +221,17 @@ rightInput.addEventListener('change', (evt) => {
   if(evt.target.value < 0) 
   rightInput.value = 0
 
-  valueX = evt.target.value
-  scaleX(valueX, valueY)
+  xScaleFuture = evt.target.value
+  scaleX(xScaleFuture, xScalePast, timeUnits, step)
  })
 
  const leftInput = document.getElementById('leftAxis')
  leftInput.addEventListener('change', (evt) => {
-  if(evt.target.value > 2)
-  leftInput.value = 2
+  if(evt.target.value < 0)
+  leftInput.value = 0
 
-  valueY= evt.target.value
-  scaleX(valueX, valueY)
+  xScalePast= evt.target.value
+  scaleX(xScaleFuture, xScalePast,timeUnits, step)
  })
 
  const datatInput = document.getElementById('dataInput')
@@ -140,10 +243,6 @@ rightInput.addEventListener('change', (evt) => {
 
 
   // if(currentData.length > 10) currentData.shift()
-
-
-
-  console.log('currentData',currentData)
   const currentLabels = chart.data.labels;
 
   // Добавляем новое значение в массив данных
@@ -158,5 +257,19 @@ rightInput.addEventListener('change', (evt) => {
 
 
 
-  scaleX(valueX,valueY )
+  scaleX(xScaleFuture,xScalePast,timeUnits, step)
  })
+
+ const selectRange = document.querySelector('#time_range')
+  selectRange.addEventListener('change', (e) => {
+    timeUnits = selectRange.value
+    scaleX(xScaleFuture,xScalePast,timeUnits, step)
+})
+
+const stepInput =  document.querySelector('#step_input')
+stepInput.addEventListener('click', (e) => {
+  if(e.target.value <= 0) e.target.value = 1
+  step = e.target.value
+    scaleX(xScaleFuture,xScalePast,timeUnits, step)
+})
+
