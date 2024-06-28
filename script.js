9a7a801f4340aa703934dd63e880af7feae69e36
Vladimir_Leonov <vladimir.leonov@systeme.ru>
2024-06-28 12:45:11 +0300

    first commit

--- script.js
+++ script.js
@@ -1,67 +1,63 @@
-// Создаем layout и добавляем в него tree-компоненты
-var layout = webix.ui({
-  rows: [
-    {
-      view: "tabbar",
-      id: "tabbar",
-      value: "treeList", // выбранная вкладка по умолчанию
+
+
+
+// webix.ui({
+//   rows:[
+//     {cols:[
+//       { 
+//         view:"tree",
+//       template:"{common.icon()} {common.checkbox()} {common.folder()} #value#",
+//       threeState: true,
+//     data: webix.copy(smalltreedata),
+//       ready:function(){
+//        this.openAll();
+//       }
+//        },
+//       {
+//         view:"tree",
+//       template:"{common.icon()} {common.checkbox()} {common.folder()} #value#",
+//     data: webix.copy(smalltreedata),
+//       ready:function(){
+//        this.openAll();
   
-      options: [
-          { id: "treeList", value: "List" },
-          { id: "treeForm", value: "Form" },
-          { id: "treeEmpty", value: "Empty" }
-      ],
-      on: {
-      
-          onChange: function (id) {
-              // Обработчик изменения вкладки
-              $$("treeList").hide();
-              $$("treeForm").hide();
-              $$("treeEmpty").hide();
-              
-              // Показываем tree, соответствующий выбранной вкладке
-              $$(id).show();
-          }
-      },
-    
-    },
+//      }}
+//     ]}
+//   ]
+// });
+
+
+
+
+
+webix.ui({
+  rows:[
+    {cols:[
       { 
-        view:"multiview",
-        cells: [
-        {
-          view: "tree",
-          id: "treeList",
-          animate:{ type:"slide",  direction: "right"  },
-          data: [
-              { id: 1, value: "Item 1", data: [] },
-              { id: 2, value: "Item 2", data: [] },
-              // Добавьте данные по вашему усмотрению
-          ],
-          
+        id: 'tree',
+        view:"treetable",
+       
+        threeState: true, // Включаем трёхсостояние чекбоксов
+        columns:[
+          { id:"value", 
+            header:"Films data", 
+            template:"{common.treetable()} {common.treecheckbox()} #value#",
+             width:300 }
+        ],
+        data: webix.copy(smalltreedata),
+        on:{
+          onCheck: function(rowId, colId, state){
+            console.log('===>',)
+            // Обновляем состояние родительских и дочерних элементов
+            updateCheckboxState(this, rowId, state);
+          },
+          onItemClick: function(id, e, node) {
+            console.log('===>item',)
+          }
         },
-        {
-          view: "tree",
-          id: "treeEmpty",
-          animate:{ type:"slide",  direction: "left"  },
-          data: [
-              { id: 1, value: "Empty Item 1", data: [] },
-              { id: 2, value: "Empty Item 2", data: [] },
-              // Добавьте данные по вашему усмотрению
-          ],
-         
-        },
-        {
-          view: "tree",
-          id: "treeForm",
-          animate:{ type:"slide",  direction: "left"  },
-          data: [
-              { id: 1, value: "Form Item 1", data: [] },
-              { id: 2, value: "Form Item 2", data: [] },
-              // Добавьте данные по вашему усмотрению
-          ],
-         
+        ready:function(){
+          this.openAll();
         }
-      ] }
+      },
+    ]}
   ]
 });
-
