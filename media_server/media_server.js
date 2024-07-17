9e2c55e5b3b946590432a69a31d10bc7474ae663
Vladimir_Leonov <vladimir.leonov@systeme.ru>
2024-07-17 16:19:29 +0300

    получил поток от медиасервера

---
+++ media_server/media_server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const https = require("https");
+const fs = require("fs");
+const path = require("path");
+const cors = require("cors");
+const config = require("./config/config");
+
+
+const app = express();
+const port = 3001;
+
+const options = {
+  key: fs.readFileSync(path.join(__dirname, "./ssl/cert.key")),
+  cert: fs.readFileSync(path.join(__dirname, "./ssl/cert.pem")),
+};
+
+app.use(cors());
+config(app);
+
+
+const server = https.createServer(options, app);
+
+server.listen(port, () => {
+  console.log(`Сервер запущен на https://127.0.0.1:${port}`);
+});
--- public/pages/real_time/real_time.js
+++ public/pages/real_time/real_time.js
@@ -14,31 +14,31 @@ window.addEventListener("beforeunload", async function (event) {
   }
 });
 
-const isManifest = async() => {
-  try{
-   const res = await fetch(`${url.protocol}//${url.hostname}:${url.port}/real_time_data/${id}/out.m3u8`)
-   if(!res.ok){
-    throw new Error('no manifest')
-   }else{
-    TITLE.innerText = `Live Stream, cam_${camNo}`;
-    loadVideo();
-   }
-  }catch(err){
-    setTimeout(isManifest, 2000)
-    console.error("Error checking file availability:", err);
-  }
-}
+// const isManifest = async() => {
+//   try{
+//    const res = await fetch(`https://localhost:3001/`)
+//    if(!res.ok){
+//     throw new Error('no manifest')
+//    }else{
+//     TITLE.innerText = `Live Stream, cam_${camNo}`;
+//     loadVideo();
+//    }
+//   }catch(err){
+//     setTimeout(isManifest, 2000)
+//     console.error("Error checking file availability:", err);
+//   }
+// }
 
 const loadVideo = async() => {
   if (Hls.isSupported()) {
     const hls = new Hls();
     hls.loadSource(
-      `${url.protocol}//${url.hostname}:${url.port}/real_time_data/${id}/out.m3u8`
+      `https://localhost:3001/out.m3u8`
     );
     hls.attachMedia(VIDEO);
     hls.on(Hls.Events.MANIFEST_PARSED, function () {});
   } else if (VIDEO.canPlayType("application/vnd.apple.mpegurl")) {
-    VIDEO.src = `${url.protocol}//${url.hostname}:${url.port}/real_time_data/${id}/out.m3u8`;
+    VIDEO.src = `https://localhost:3001/out.m3u8`;
     VIDEO.addEventListener("canplay", function () {
       VIDEO.play().catch((error) => {
         console.error("Error playing video:", error);
@@ -46,5 +46,6 @@ const loadVideo = async() => {
     });
   }
 }
-isManifest()
+loadVideo()
+//isManifest()
 
