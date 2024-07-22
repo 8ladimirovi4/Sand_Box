2a00b38ab3817a8efc6f5486a01022c525fbc3ff
Vladimir_Leonov <vladimir.leonov@systeme.ru>
2024-07-22 13:49:35 +0300

    add dementions for realtime

--- routes/indexRouter.js
+++
@@ -1,8 +0,0 @@
-const indexRouter = require("express").Router();
-const path = require("path");
-
-indexRouter.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/pages/index", "index.html"));
-});
-
-module.exports = indexRouter;
