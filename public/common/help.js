d7a42c8a3598497fb25ea25fba6956381ad435c5
Vladimir_Leonov <vladimir.leonov@systeme.ru>
2024-07-22 13:51:52 +0300

    add dementions for realtime

---
+++ public/common/help.js
@@ -0,0 +1,19 @@
+const GUID = {
+  EMPTY: "00000000-0000-0000-0000-000000000000",
+  isValid: function (val) {
+    let pattern = new RegExp(
+      "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$"
+    );
+    return pattern.test(val);
+  },
+  newID: function () {
+    let s = [];
+    let hexDigits = "0123456789abcdef";
+    for (let i = 0; i < 36; i++)
+      s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1);
+    s[14] = "4";
+    s[19] = hexDigits.substr((s[19] & 0x3) | 0x8, 1);
+    s[8] = s[13] = s[18] = s[23] = "-";
+    return s.join("");
+  },
+};
---
+++ public/pages/real_time/real_time.js
@@ -0,0 +1,60 @@
+const url = new URL(location.href);
+const searchParams = new URLSearchParams(url.search);
+const id = searchParams.get("id");
+const camNo = searchParams.get("camNo");
+const VIDEO = document.getElementById("video");
+const TITLE = document.querySelector(".container_title");
+const radioButtons = document.querySelectorAll('input[name="contact"]');
+
+//звершить ffmpeg процесс
+window.addEventListener("beforeunload", async function (event) {
+  try {
+    await fetch(`/real_time/stop_ffmpeg_process/${id}`);
+  } catch (error) {
+    console.error("Error stopping ffmpeg process:", error);
+  }
+});
+
+const checkFileAvailability = async () => {
+  try {
+    const response = await fetch(
+      `${url.protocol}//${url.hostname}:${url.port}/real_time/check_file/${id}`
+    );
+    if (response.ok) {
+      TITLE.innerText = `Live Stream, cam_${camNo}`;
+      loadVideo();
+    } else {
+      setTimeout(checkFileAvailability, 2000); // Повторить через 2 секунды
+    }
+  } catch (error) {
+    console.error("Error checking file availability:", error);
+    setTimeout(checkFileAvailability, 2000); // Повторить через 2 секунды
+  }
+};
+
+function loadVideo() {
+  if (Hls.isSupported()) {
+    const hls = new Hls();
+    hls.loadSource(
+      `${url.protocol}//${url.hostname}:${url.port}/real_time_data/${id}/out.m3u8`
+    );
+    hls.attachMedia(VIDEO);
+    hls.on(Hls.Events.MANIFEST_PARSED, function () {});
+  } else if (VIDEO.canPlayType("application/vnd.apple.mpegurl")) {
+    VIDEO.src = `${url.protocol}//${url.hostname}:${url.port}/real_time_data/${id}/out.m3u8`;
+    VIDEO.addEventListener("canplay", function () {
+      VIDEO.play().catch((error) => {
+        console.error("Error playing video:", error);
+      });
+    });
+  }
+}
+checkFileAvailability();
+
+radioButtons.forEach(radio => {
+    radio.addEventListener('change', function() {
+        const [width, height] = this.value.split('x');
+        VIDEO.style.width = width + 'px';
+        VIDEO.style.height = height + 'px';
+    });
+});
