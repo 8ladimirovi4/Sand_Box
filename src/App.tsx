2d7aeae25857f047487613b6408f17ae2bf31471
Vladimir_Leonov <vladimir.leonov@systeme.ru>
2025-02-01 16:09:37 +0300

    init: init project

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import './App.css';
+import 'styles/output.css';
+
+function App() {
+  return <h1>установил Tailwind</h1>;
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { fileURLToPath, URL } from 'node:url';
+import path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      assets: path.resolve(__dirname, 'src/assets'),
+      components: path.resolve(__dirname, 'src/components'),
+      pages: path.resolve(__dirname, 'src/pages'),
+      store: path.resolve(__dirname, 'src/store'),
+      styles: path.resolve(__dirname, 'src/styles'),
+    },
+  },
+});
