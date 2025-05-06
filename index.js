6a3b24e5907932025e669c6c561126cfaf8999c4
Vladimir_Leonov <vladimir.leonov@systeme.ru>
2025-05-06 19:32:02 +0300

    добавил функицонал вызова метода без скрипта

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const serverTags = {
 
 const figureScripts = {
       scriptCode: `
-        if(tags.isOn === 'on' && tags.rotate > 50){
-          methods.rotate("c7f990f9-7608-4f73-918f-15647c7ae53b", 50);
+        if(tags.isOn === 'on' && tags.rotate > 50){ 
+          methods.setAngle("c7f990f9-7608-4f73-918f-15647c7ae53b", 50);
           methods.setBorderColor("c7f990f9-7608-4f73-918f-15647c7ae53b", "green");
         }
-        if (tags.isOpen) {
-          methods.setBackgroundColor("c7f990f9-7608-4f73-918f-15647c7ae53b", "red");
+        if (tags.isOpen && tags.scale === 50) {
+          methods.setAngle("c7f990f9-7608-4f73-918f-15647c7ae53b", 120);
         }
         if (tags.rotate > 75) {
-          methods.rotate("ceacf5f2-2f4c-40b6-8b1b-5bd34f47b4ee", 45);
+          methods.setAngle("ceacf5f2-2f4c-40b6-8b1b-5bd34f47b4ee", 45);
         }
         if(tags.scale > 50){
           methods.setBackgroundColor("6495a835-e5c2-4a1e-aed5-c6aee253f044", "yellow");
@@ -28,10 +28,14 @@ const figureScripts = {
         rotate: "Tank_1_level", //75
         isOpen: "Tank_2_open", // true
         scale: "Tank_2_level" //50
+      },
+      methodTriggers: {
+        isOpen: `methods.setBorderColor("c7f990f9-7608-4f73-918f-15647c7ae111", "purpule")`
       }
   };
   
-const methods = {
+
+  const methods = {
     setBackgroundColor: (id, color) => {
         console.log('===>id setBackgroundColor', id)
         console.log('===>color setBackgroundColor',color)
@@ -42,13 +46,13 @@ const methods = {
         console.log('===>color setBorderColor',color)
         return { action: 'setBorderColor', color }
     },
-    rotate: (id, angle) => {
-        console.log('===>rotate id', id)
-        console.log('===>rotate angle',angle)
-        return { action: 'rotate', angle }
+    setAngle: (id, angle) => {
+        console.log('===>setAngle id', id)
+        console.log('===>setAngle angle',angle)
+        return { action: 'setAngle', angle }
     },
   };
-
+  
   function runScript(scriptCode, tags, methods) {
     const wrapper = `
       "use strict";
@@ -62,6 +66,31 @@ const methods = {
     } catch (err) {
       console.error("Ошибка при выполнении скрипта:", err.message);
       return null;
+    }
+  }
+
+  function runMethod (method) {
+    const wrapper = `
+    "use strict";
+    return (function(method) {
+      ${method}
+    })(method);
+  `;
+        try {
+            const fn = new Function("method", wrapper)
+            fn(method)
+        } catch (err) {
+            console.error("Ошибка при выполнении метода:", err.message);
+            return null;
+        }
+  }
+
+function applyMethodTriggers(methodTriggers, tags) {
+    for (const field in methodTriggers) {
+      if (tags[field] === true) {
+        const method = methodTriggers[field];
+        runMethod(method)
+      }
     }
   }
   
@@ -78,8 +107,10 @@ const methods = {
     const figureId = figureJson.attributes?.id;
     if (!figureId) return;
   
-    const { scriptCode, internalTags } = figureScripts;
+    const { scriptCode, internalTags, methodTriggers } = figureScripts;
     const tags = resolveInternalTags(internalTags, serverTags);
+
+    applyMethodTriggers(methodTriggers, tags);
     runScript(scriptCode, tags, methods);
   }
   
