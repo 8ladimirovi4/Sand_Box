237d0ba5c5bde9238df2a031fa5d49fe9ba9c32a
Vladimir_Leonov <vladimir.leonov@systeme.ru>
2025-06-05 21:03:00 +0300

    fix: task5

--- task5/task.js
+++ task5/task.js
@@ -15,15 +15,16 @@ rl.on("close", function () {
   const prices = input[0].split(/\s+/).map(Number);
   const S = input[1];
 
-  //n = 6
+  //n = 6 длина строки
   //a = 4 стоимость обмена скобок местами
   //b = 3 стоисмость заметы скобки
   //S = ())(((
+  
   const n = prices[0];
   const a = prices[1];
   const b = prices[2];
 
-  function analizeBalance(strLength, str) {
+  function analizeBalance(str) {
     let balance = 0;
     let unmutchedClose = 0;
 
@@ -46,13 +47,13 @@ rl.on("close", function () {
   const unmutchedScopes = analizeBalance(n, S);
 
   function calculateMinCost(scopes, swapCost, changeCost) {
-    const { unmutchedClose, unmatchedOpen } = unmutchedScopes;
+    const { unmutchedClose, unmatchedOpen } = scopes;
 
     const swaps = Math.min(unmutchedClose, unmatchedOpen);
     const remainingOpen = unmutchedClose - swaps;
     const remainingClose = unmatchedOpen - swaps;
 
-    return swaps * swapCost + ((remainingOpen + remainingClose) / 2) * b;
+    return swaps * swapCost + ((remainingOpen + remainingClose) / 2) * changeCost;
   }
 
   console.log(calculateMinCost(unmutchedScopes, a, b));
