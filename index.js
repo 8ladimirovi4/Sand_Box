fdf43d9ba41fbacc7beba83a5318dd547704bcd9
Vladimir_Leonov <2227331.spb@gmail.com>
2025-06-22 17:17:28 +0300

    fix: camera data

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ for (let i = 0; i < userPhotos.length; i++) {
 
 const photo1 = new Photo('01.01.0001', 'base64pic', 50)
 const photo2 = new Photo('02.02.0002', 'base64pic', 40)
-const camera = new Camera(100, [photo1, photo2])
+const camera = new Camera(100)
 
 console.log('===> camera.memory', camera.memory)
 console.log('===> camera.photos', camera.photos)
