179a6d26a7ea82d98cd87531f7207293e7caeeb5
Vladimir_Leonov <2227331.spb@gmail.com>
2025-06-22 19:15:10 +0300

    add classes polymorphism

--- index.js
+++ index.js
@@ -1,24 +1,67 @@
-class Country {
-  constructor(cities){
-    this.cities = cities
-  }
-}
+// полиморфизм — это способность объектов с одинаковым интерфейсом (одинаковыми именами методов) иметь разную реализацию (разное поведение) этих методов.
 
-class City {
-  constructor(name, latitude, longitude){
+class Character {
+  constructor(name, health, strength){
     this.name = name;
-    this.latitude = latitude;
-    this.longitude = longitude
+    this.health = health;
+    this.strength = strength;
+  }
+  showStatus(){
+    return `герой: \n${this.name} \nздоровье: ${this.health} \nсила ${this.strength}`
   }
 }
 
-class CapitalCity extends City{
-  constructor(name, latitude, longitude, country){
-    super(name, latitude, longitude)
-    this.contry = country
+const hero = new Character('Goblin', 50, 10)
+
+console.log('===> ', hero.showStatus())
+
+class Potion {
+  constructor(name){
+    this.name = name
+  }
+
+  drink(character){
+     console.log(`герой ${character.name} выпивает ${this.name}`)
+      this._applyEffect(character);
+  }
+   _applyEffect() {
+    throw new Error("Метод _applyEffect должен быть реализован в дочернем классе!");
   }
 }
 
-const berlin = new CapitalCity('Berlin', 1.2345, 2.4567, 'Germany')
+class HealthPotion extends Potion {
+  constructor(){
+    super('Health Potion')
+  }
+  _applyEffect(character){
+    return character.health += 10
+  }
+}
 
-console.log('===> ', berlin)
+class StrengthPotion extends Potion {
+  constructor(){
+    super('Strength Potion')
+  }
+  _applyEffect(character){
+    return character.strength += 20
+  }
+}
+
+class Sword {
+  constructor(){
+    this.name = 'Sword'
+  }
+}
+
+const bagWithPotions = [
+  new HealthPotion(),
+  new StrengthPotion(),
+  new Sword ()
+]
+
+bagWithPotions.forEach(potion => {
+ if(potion instanceof Potion)
+  potion.drink(hero)
+})
+
+console.log('===> ', hero.showStatus())
