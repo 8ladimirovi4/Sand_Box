ca0e7eeacd59fcea3a5bd8ee3a3dcc92a33eb9ac
Vladimir_Leonov <2227331.spb@gmail.com>
2025-06-22 17:16:41 +0300

    add some private methods

--- index.js
+++ index.js
@@ -10,13 +10,27 @@ class Camera {
   #photos
   #memory
 
-  constructor(memory, photos){
+  constructor(memory){
     this.#memory = memory
-    this.#photos = photos
+    this.#photos = []
+  }
+
+  #add(userPhotos) {
+    if(!Array.isArray(userPhotos)) return 
+
+for (let i = 0; i < userPhotos.length; i++) {
+   this.#memory -= userPhotos[i].volume
+  if(this.#memory < 0) break;
+  this.#photos.push(userPhotos[i])
+}
   }
 
   #remove() {
      this.#photos.pop()
+  }
+
+  addPhoto(userPhotos){
+       this.#add(userPhotos) 
   }
 
   removePhoto(){
@@ -28,21 +42,22 @@ class Camera {
   }
 
   get memory(){
-    this.#photos.forEach(photo => {
-          this.#memory -= photo.volume
-    })
     return this.#memory
   }
 
 }
 
-const photo1 = new Photo('01.01.0001', 'base64pic', 10)
-const photo2 = new Photo('02.02.0002', 'base64pic', 20)
+const photo1 = new Photo('01.01.0001', 'base64pic', 50)
+const photo2 = new Photo('02.02.0002', 'base64pic', 40)
 const camera = new Camera(100, [photo1, photo2])
 
 console.log('===> camera.memory', camera.memory)
-console.log('===> camera.photos1', camera.photos)
+console.log('===> camera.photos', camera.photos)
+
+camera.addPhoto([photo1, photo2])
+console.log('===> camera.photos', camera.photos)
+console.log('===> camera.memory', camera.memory)
 
 camera.removePhoto()
 
-console.log('===> camera.photos2', camera.photos)
+console.log('===> camera.photos', camera.photos)
