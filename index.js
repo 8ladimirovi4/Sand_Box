ce1704b843b7f15038b8f2992cd079174062086f
Vladimir_Leonov <2227331.spb@gmail.com>
2025-08-07 23:37:54 +0300

    add quick sort

--- index.js
+++ index.js
@@ -465,4 +465,26 @@ function groupAnagrams(strs) {
 }
 
 console.log('===> ',groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]) )
+//------------------------------------------------------------------//
+
+//Quick sort
+function quickSort(arr) {
+  if (arr.length <= 1) return arr;
+
+  const pivot = arr[arr.length - 1];
+  const left = [];
+  const right = [];
+
+  for (let i = 0; i < arr.length - 1; i++) { // проход по всем элементам кроме pivot
+    if (arr[i] < pivot) {
+      left.push(arr[i]);
+    } else {
+      right.push(arr[i]);
+    }
+  }
+
+  return [...quickSort(left), pivot, ...quickSort(right)];
+}
+
+console.log('===> ', quickSort([10, 8, 2, 1, 6]));
  
