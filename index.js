532240452c015caccc3fa2781df1c26392b1e29d
Vladimir_Leonov <2227331.spb@gmail.com>
2025-08-12 23:46:00 +0300

    feat: add integerToRoman

--- index.js
+++ index.js
@@ -681,3 +681,37 @@ function romanToInteger(str) {
 console.log('===>,', romanToInteger('LXXII')); //72
 console.log('===>,', romanToInteger('IVIV')); //8
 console.log('===> ', romanToInteger('MCMXCIV')); //1994
+
+//------------------------------------------------------------------//
+//integerToRoman
+
+const romanDict = {
+  1: 'I',
+  4: 'IV',
+  5: 'V',
+  9: 'IX',
+  10: 'X',
+  40: 'XL',
+  50: 'L',
+  90: 'XC',
+  100: 'C',
+  400: 'CD',
+  500: 'D',
+  900: 'CM',
+  1000: 'M',
+}
+
+const integerToRoman = (number) => {
+  const arr = Object.keys(romanDict).sort((a, b) => b - a);
+  let result = '';
+
+  let divisionResult = 0
+  
+  for (let el of arr) {
+    divisionResult = Math.floor(number / Number(el));
+    result += romanDict[el].repeat(divisionResult);
+    number = number % el;
+  }
+
+  return result;
+}
